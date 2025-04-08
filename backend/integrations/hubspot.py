b8e52dee29044f7552298ee77aa9383f2653dda6
Mayank Gupta <mayank.gupta8@stryker.com>
2025-04-08 13:30:48 +0530

    first commit

---
+++ backend/integrations/hubspot.py
@@ -0,0 +1,29 @@
+# slack.py
+
+from fastapi import Request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+CLIENT_ID = os.getenv("HUBSPOT_CLIENT_ID")
+CLIENT_SECRET = os.getenv("HUBSPOT_CLIENT_SECRET")
+
+async def authorize_hubspot(user_id, org_id):
+    # TODO
+    pass
+
+async def oauth2callback_hubspot(request: Request):
+    # TODO
+    pass
+
+async def get_hubspot_credentials(user_id, org_id):
+    # TODO
+    pass
+
+async def create_integration_item_metadata_object(response_json):
+    # TODO
+    pass
+
+async def get_items_hubspot(credentials):
+    # TODO
+    pass
